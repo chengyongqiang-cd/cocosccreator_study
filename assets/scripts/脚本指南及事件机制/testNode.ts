5dbd511459f12e858565ccb38f73c02aaa66f7b4
Chances Cheng <cyqforwork@163.com>
2024-05-08 17:20:48 +0800

    create&destroy
    创建和销毁节点

--- assets/scripts/脚本指南及事件机制/testNode.ts
+++ assets/scripts/脚本指南及事件机制/testNode.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Label, Node } from 'cc';
+import { _decorator, Component, Label, Node, Vec3 } from 'cc';
 import { Global } from './Global';
 const { ccclass, property } = _decorator;
 
@@ -8,13 +8,39 @@ export class testNode extends Component {
         this.node.setScale(1, 1, 1)
 
         let node = this.node
-        node.setPosition(20, 20, 20)
+        // node.setPosition(20, 20, 20)
+
+        this.setPosition()
+
+        this.setNodeRota()
     }
 
 
 
     update(deltaTime: number) {
-        this.node.setPosition(0, 40 * deltaTime, 0)
+        //this.node.setPosition(0, 40 * deltaTime, 0)
+        this.node.position = new Vec3(0, deltaTime * 2, 0)
+    }
+
+    //     设置节点位置
+    setPosition() {
+        this.node.setPosition(-100, 50, 100)
+        this.node.setPosition(new Vec3(-100, 50, 100))
+        this.node.position = new Vec3(-100, 50, 100)
+        console.log(this.node.position)
+    }
+
+    //设置节点旋转
+    setNodeRota() {
+        //this.node.setRotation(0, 90, 0, 0)
+        this.node.setRotationFromEuler(90, 90, 90);
+        setTimeout(() => {
+            this.node.destroy()
+        }, 6000)
+    }
+
+    protected onDestroy(): void {
+        console.log('该节点被销毁了')
     }
 }
 
--- assets/scripts/脚本指南及事件机制/testScript.ts
+++ assets/scripts/脚本指南及事件机制/testScript.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Vec3, Label, Color } from 'cc';
+import { _decorator, Component, Node, Vec3, Label, Color, director, instantiate } from 'cc';
 import { Global } from './Global';
 const { ccclass, property } = _decorator;
 
@@ -22,6 +22,10 @@ export class testScript extends Component {
 
         this.findChild()
         this.setGlobalNode()
+
+        this.createNode()
+
+        this.cloneNode()
     }
 
     update(deltaTime: number) {
@@ -47,12 +51,37 @@ export class testScript extends Component {
         }, 3000)
     }
 
-    //查找子节点
+    //查找子节点B
     findChild() {
         let childs = this.node.children
         console.log(childs)
         let node = this.node.getChildByName('Node')
         console.log(node.name)
     }
+
+    //动态创建节点
+    createNode() {
+        let node = new Node('createNode')
+        //director.getScene().addChild(node)
+        this.node.addChild(node)
+        const label = node.addComponent(Label)
+        label.color = new Color('#000000')
+        label.string = '新添加了一个节点'
+
+        node.setPosition(2, 2, 0)
+    }
+
+    //克隆节点
+    cloneNode() {
+        let node = instantiate(this.labelTestNode)
+        this.node.addChild(node)
+        node.setPosition(60, 60, 0)
+
+        setTimeout(() => {
+            node.destroy()
+        }, 5000)
+    }
+
+
 }
 
