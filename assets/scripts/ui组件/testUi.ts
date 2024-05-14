d809d25c5609388881470fa64f175265b2f342b6
Chances Cheng <cyqforwork@163.com>
2024-05-14 17:06:05 +0800

    Ui组件测试

--- assets/scripts/ui组件/testUi.ts
+++ assets/scripts/ui组件/testUi.ts
@@ -1,14 +1,37 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Button, Component, Node, Widget } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('testUi')
 export class testUi extends Component {
-    start() {
+    @property({ type: Node })
+    testWightNode: Node = null!
+    @property({ type: Node })
+    testButton: Node = null!
 
+    protected onLoad(): void {
+        this.testButton.on(Button.EventType.CLICK, this.buttonClick, this)
+    }
+
+    protected onDestroy(): void {
+        this.testButton.off(Button.EventType.CLICK, this.buttonClick, this)
+    }
+    start() {
+        this.setWightCom()
     }
 
     update(deltaTime: number) {
-        
+
     }
+
+    setWightCom() {
+        let widget = this.testWightNode.addComponent(Widget)
+        widget.top = 50
+    }
+
+    buttonClick() {
+        console.log('按钮点击了')
+    }
+
+
 }
 
